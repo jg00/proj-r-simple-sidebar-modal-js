df33b90caeb1cd07c783e81d8ce2cc6d1be17549
Jack Gamboa <jgamboa929@gmail.com>
2022-03-22 09:33:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Simple Sidebar and Modal with React
+
+Simple sidebar and modal with React. Explore Context API and useContext.
+
+![react-context-api-and-usecontext](https://user-images.githubusercontent.com/43181662/159505995-4128fdc2-9121-4832-afed-f60e0e263e82.png)
+
+## Technologies
+
+:coffee: **_React_**
---
+++ src/context.js
@@ -0,0 +1,48 @@
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
+
+/*
+React hooks can only be called either in a React component or in a custom hook.
+In order to be a custom hook that function needs to start with useMyCustomHook
+*/
